a61b35d4727d8b607e43c2fa679a64a4dd0df853
Dave Syer <dsyer@pivotal.io>
2020-02-26 13:57:58 +0000

    Use 1G memory

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ echo Use 'CTRL-A C' to switch to monitor.
 if qemu-img snapshot -l disk.qcow | grep init; then
     snapshot="-loadvm init"
 fi
-qemu-system-x86_64 -hda disk.qcow -enable-kvm -net nic -net user,hostfwd=tcp::2222-:22,hostfwd=tcp::8080-:8080 -localtime -m 4096 -nographic $snapshot
+qemu-system-x86_64 -hda disk.qcow -enable-kvm -net nic -net user,hostfwd=tcp::2222-:22,hostfwd=tcp::8080-:8080 -localtime -m 1024 -nographic $snapshot
 
 # KVM notes:
 # $ sudo usermod -aG kvm $(whoami)
