d736b61ef164f2770ee97b5a0c6606036044a1e0
Dave Syer <dsyer@pivotal.io>
2020-03-31 11:59:13 +0100

    Strip down packages and add non-root user

--- example/configure.sh
+++ example/configure.sh
@@ -21,13 +21,12 @@ ln -s networking /etc/init.d/net.eth0
 step 'Adjust rc.conf'
 sed -Ei \
 	-e 's/^[# ](rc_depend_strict)=.*/\1=NO/' \
-	-e 's/^[# ](rc_logger)=.*/\1=YES/' \
+	-e 's/^[# ](rc_logger)=.*/\1=YES/' \qq
 	-e 's/^[# ](unicode)=.*/\1=YES/' \
 	/etc/rc.conf
 
 step 'Enable services'
 rc-update add acpid default
-rc-update add chronyd default
 rc-update add crond default
 rc-update add sshd default
 rc-update add net.eth0 default
@@ -41,6 +40,20 @@ PATH=\$PATH:\$JAVA_HOME/bin
 EOF
 
 step 'Adjust sshd_conf'
-echo root:root | chpasswd
-sed -i -e 's/.PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+AUSER=dsyer
+adduser -D -u 1000 -s /bin/bash $AUSER $AUSER
+adduser $AUSER wheel
+sed -e 's;^# \(%wheel.*NOPASSWD.*\);\1;g' -i /etc/sudoers
+sed -i -e "s/$AUSER:!/$AUSER:*/" /etc/shadow
+mkdir -p /home/$AUSER/.ssh
+chown $AUSER:$AUSER /home/$AUSER/.ssh
+chmod 700 /home/$AUSER/.ssh
+cat << EOF > /home/$AUSER/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA4yvDJ+mZfpMbCaPuZJ4jVmFHbTiN3ksCOvWj/4n9occuM0hMSqGlIvQ3686XB2ZUmxnN7Z4LGNS4eFYTdRZ6XXoEfdXXJBKMKLZwr5YBJasIV7bBiTFjX6lJDOkzRK0G5qyjO29z2nW3JfoReBXLzOOITuLj0bjWZTUsdrQ4tmOlPUtVe30yql/06YWcdn0jII1PASDw2yrRvbeOFM/nig3zElzb6+m8V5Y9BQ5HyDd6sdMTCwWiYWC1/S6EOmvB3HadbeNdH4LjoEgXGBJ/6u5icavpWQOmFQ5M/ZLkkfokkCIQQEIUHdFVGx5y4myvVPWEGQ79aOpVrBr1WxaJDw== dsyer_rsa
+EOF
+chown $AUSER:$AUSER /home/$AUSER/.ssh/*
+chmod 600 /home/$AUSER/.ssh/*
 
+#echo root:root | chpasswd
+#sed -i -e 's/.PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i -e 's/.PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
